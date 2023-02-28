8301fef61367327b79fd8f82d53810cce8445abf
david mendez <87deivid@gmail.com>
2023-02-27 23:11:59 -0300

    primer version

---
+++ java/java.js
@@ -0,0 +1,8 @@
+
+//function esconder () {
+    //document.getElementById("demo").style.display = "none";
+ // } 
+
+  function mostrar () {
+    document.getElementById("demo").style.display = "inline";
+  }
